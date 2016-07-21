8f2de4cbcf857afcd0e88b3f36e30b8ccc9d53a0
Berelindis <romain.bellande@orange.fr>
2016-07-22 00:22:04 +0200

    add level for creeps

--- creeps.js
+++ creeps.js
@@ -6,36 +6,51 @@ var creeps = {
 
   },
   type: {
-   harvester:  [MOVE, CARRY, WORK],
-   healer: [HEAL, MOVE, CARRY]
- },
- counter: 0,
- creepCost: function (body) {
-  var bodyCost = {
-    "move": 50,
-    "carry": 50,
-    "work": 20,
-    "heal": 200,
-    "tough": 20,
-    "attack": 80,
-    "ranged_attack": 150
-  };
-  var cost = 0;
-  _.forEach(body, function(part) { cost += bodyCost[part]; });
-  return cost;
-},
+    harvester: {
+      counter: 0,
+      level: [
+        [MOVE, CARRY, WORK],
+        [MOVE, MOVE, CARRY, WORK],
+      ],
+    },
+    upgrader: {
+      counter: 0,
+      level: [
+        [MOVE, CARRY, WORK],
+        [MOVE, MOVE, CARRY, WORK],
+      ],
+    },
+    healer: [[HEAL, MOVE, CARRY]]
+  },
+  creepCost: function (body) {
+    var bodyCost = {
+      "move": 50,
+      "carry": 50,
+      "work": 20,
+      "heal": 200,
+      "tough": 20,
+      "attack": 80,
+      "ranged_attack": 150
+    };
+    var cost = 0;
+    _.forEach(body, function(part) { cost += bodyCost[part]; });
+    return cost;
+  },
 
-createCreep: function (type) {
-  console.log('types', this.type[type]);
-  console.log('creepCost', this.creepCost(this.type[type]));
-  var spawn = core.spawns.getEnergyAbove(this.creepCost(this.type[type]))[0];
-  if (spawn) {
-    spawn.createCreep(this.type[type], type + '_' + this.counter, {role: type});
-    this.counter ++;
+  createCreep: function (type, level) {
+    if (!level) level = 0;
+    console.log('Attempt to create a ' + type  +' level ' + level +
+      '. Body: ' + this.type[type].level[level] +
+      '. Cost: ' +  this.creepCost(this.type[type].level[level]) +
+      '. Counter: ' + this.type[type].counter);
+    var spawn = core.spawns.getEnergyAbove(this.creepCost(this.type[type].level[level]))[0];
+    if (spawn) {
+      spawn.createCreep(this.type[type].level[level], type + '_' + this.type[type].counter, {role: type});
+      this.type[type].counter += 1 ;
+    }
+  },
+  autoSpawn: function () {
+    this.createCreep('upgrader');
   }
-},
-autoSpawn: function () {
-  this.createCreep('harvester');
-}
 };
 module.exports = creeps;
